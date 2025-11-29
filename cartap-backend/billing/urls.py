b59b79cf92220ab01da3a8425bd9abe6533d0fa7
IslamKalykov <islamkalykov1@gmail.com>
2025-11-30 01:16:26 +0600

    Добавил локации

--- cartap-backend/billing/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import PlanListView, MockPayView, MySubscriptionsView, CurrentPlanView
-
-urlpatterns = [
-    path("plans/", PlanListView.as_view(), name="billing-plans"),
-    path("mock-pay/", MockPayView.as_view(), name="billing-mock-pay"),
-    path("my/", MySubscriptionsView.as_view(), name="billing-my"),
-    path("current/", CurrentPlanView.as_view(), name="billing-current"),  # 🔹
-]
